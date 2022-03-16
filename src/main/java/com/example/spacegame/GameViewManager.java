13b9fc56e8315bb34f7e934ae18b7f7c6616f2cb
bryan <bigavilanezb00@elpuig.xeill.net>
2022-03-16 02:13:51 +0100

    SpaceGame 5

--- src/main/java/com/example/spacegame/GameViewManager.java
+++ src/main/java/com/example/spacegame/GameViewManager.java
@@ -11,6 +11,8 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import org.controlsfx.control.spreadsheet.Grid;
 
+import java.util.Random;
+
 public class GameViewManager {
 
     private AnchorPane gamePane;
@@ -32,9 +34,17 @@ public class GameViewManager {
     private GridPane gridPane2;
     private final static String BACKGROUND_IMAGE = "file:img/darkPurple.png";
 
+    private final static String METEORO_MARRON_IMAGE = "file:img/meteorBrown_big4.png";
+    private final static String METEORO_GRIS_IMAGE = "file:img/meteorGrey_big4.png";
+
+    private ImageView[] MeteorosMarrones;
+    private ImageView[] MeteorosGrises;
+    Random randomPositionGenerator;
+
     public GameViewManager() {
         initializaStage();
         createKeyListeners();
+        randomPositionGenerator = new Random();
     }
 
     private void createKeyListeners() {
@@ -72,12 +82,61 @@ public class GameViewManager {
         gameStage.setScene(gameScene);
     }
 
-    public void createNewGame(Stage menuStage, Ship choosenChip){
+    public void createNewGame(Stage menuStage, Ship choosenShip){
         this.menuStage = menuStage;
         this.menuStage.hide();
-        createShip(choosenChip);
+        createBackground();
+        createShip(choosenShip);
+        createGameElements(choosenShip);
         createGameLoop();
         gameStage.show();
+    }
+
+    private void createGameElements( Ship choosenShip){
+        MeteorosMarrones = new ImageView[3];
+        for (int i = 0; i < MeteorosMarrones.length; i++){
+            MeteorosMarrones[i] = new ImageView(METEORO_MARRON_IMAGE);
+            setNewElementsPosition(MeteorosMarrones[i]);
+            gamePane.getChildren().add(MeteorosMarrones[i]);
+        }
+
+        MeteorosGrises = new ImageView[3];
+        for (int i = 0; i < MeteorosGrises.length; i++){
+            MeteorosGrises[i] = new ImageView(METEORO_GRIS_IMAGE);
+            setNewElementsPosition(MeteorosGrises[i]);
+            gamePane.getChildren().add(MeteorosGrises[i]);
+        }
+    }
+
+    private void moveGameElements(){
+        for (int i = 0; i < MeteorosMarrones.length; i++){
+            MeteorosMarrones[i].setLayoutY(MeteorosMarrones[i].getLayoutY()+7);
+            MeteorosMarrones[i].setRotate(MeteorosMarrones[i].getRotate()+4);
+        }
+        for (int i = 0; i < MeteorosGrises.length; i++){
+            MeteorosGrises[i].setLayoutY(MeteorosGrises[i].getLayoutY()+7);
+            MeteorosGrises[i].setRotate(MeteorosGrises[i].getRotate()+4);
+        }
+    }
+
+    private void checkIfElementsAreBehindTheShipAndRelocate(){
+
+        for (int i = 0; i < MeteorosMarrones.length; i++){
+            if (MeteorosMarrones[i].getLayoutY() > 900) {
+                setNewElementsPosition(MeteorosMarrones[i]);
+            }
+        }
+
+        for (int i = 0; i < MeteorosGrises.length; i++){
+            if (MeteorosGrises[i].getLayoutY() > 900){
+                setNewElementsPosition(MeteorosGrises[i]);
+            }
+        }
+    }
+
+    private void setNewElementsPosition(ImageView image) {
+        image.setLayoutX(randomPositionGenerator.nextInt(370));
+        image.setLayoutY(-(randomPositionGenerator.nextInt(3200)+600));
     }
 
     private void createShip(Ship choosenShip) {
@@ -91,6 +150,9 @@ public class GameViewManager {
         gameTimer = new AnimationTimer() {
             @Override
             public void handle(long l) {
+                moveBackground();
+                moveGameElements();
+                checkIfElementsAreBehindTheShipAndRelocate();
                 moveShip();
             }
         };
@@ -143,6 +205,31 @@ public class GameViewManager {
     private void createBackground() {
         gridPane1 = new GridPane();
         gridPane2 = new GridPane();
+
+        for (int i = 0; i < 12; i++){
+            ImageView backgroundImage1 = new ImageView(BACKGROUND_IMAGE);
+            ImageView backgroundImage2 = new ImageView(BACKGROUND_IMAGE);
+            GridPane.setConstraints(backgroundImage1, i% 3, i /3);
+            GridPane.setConstraints(backgroundImage2, i% 3, i / 3);
+            gridPane1.getChildren().add(backgroundImage1);
+            gridPane2.getChildren().add(backgroundImage2);
+        }
+
+        gridPane2.setLayoutY(-1024);
+
+        gamePane.getChildren().addAll(gridPane1,gridPane2);
+    }
+
+    private void moveBackground() {
+        gridPane1.setLayoutY(gridPane1.getLayoutY() + 0.5);
+        gridPane2.setLayoutY(gridPane2.getLayoutY() + 0.5);
+
+        if (gridPane1.getLayoutY() >= 1024){
+            gridPane1.setLayoutY(-1024);
+        }
+        if (gridPane2.getLayoutY() >= 1024){
+            gridPane2.setLayoutY(-1024);
+        }
 
     }
 
